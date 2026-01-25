0199dc9194223dc4c601044480b9092800e8faf4
j-adenilson <j.adenilson@outlook.com.br>
2026-01-24 23:53:06 -0300

    feat: Migrations e relacionamento entre tabelas

---
+++ src/main/java/com/movieflix/controller/MovieController.java
@@ -0,0 +1,14 @@
+package com.movieflix.controller;
+
+import com.movieflix.service.MovieService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/movieflix/movie")
+@RequiredArgsConstructor
+public class MovieController {
+    private final MovieService movieService;
+
+}
